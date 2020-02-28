0929e7952628f0bcbb6eb959653a039ef945c79c
dimashpk <shupimonstr@gmail.com>
2020-02-28 22:02:18 +0200

    Updates

---
+++ precache-manifest.c6aae162ad9641249030ce74436a9ae1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bf1e1a2024879320fc649a40389a911",
+    "url": "/TestWork/index.html"
+  },
+  {
+    "revision": "54f5438311b95f0768cb",
+    "url": "/TestWork/static/css/2.4121280a.chunk.css"
+  },
+  {
+    "revision": "3b12779048ed4498e881",
+    "url": "/TestWork/static/css/main.f8fb6ca2.chunk.css"
+  },
+  {
+    "revision": "54f5438311b95f0768cb",
+    "url": "/TestWork/static/js/2.47061629.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/TestWork/static/js/2.47061629.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b12779048ed4498e881",
+    "url": "/TestWork/static/js/main.f5ac1aa3.chunk.js"
+  },
+  {
+    "revision": "733a6446e4b0abb09d98",
+    "url": "/TestWork/static/js/runtime-main.b05e164c.js"
+  },
+  {
+    "revision": "31d98c8d4ef07798144ab14521b0b818",
+    "url": "/TestWork/static/media/OPTIHobble-OldStyle.31d98c8d.eot"
+  },
+  {
+    "revision": "31d98c8d4ef07798144ab14521b0b818",
+    "url": "/TestWork/static/media/OPTIHobble-OldStyle.31d98c8d.ttf"
+  },
+  {
+    "revision": "31d98c8d4ef07798144ab14521b0b818",
+    "url": "/TestWork/static/media/OPTIHobble-OldStyle.31d98c8d.woff"
+  }
+]);
